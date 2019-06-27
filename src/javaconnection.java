c90a85b41cf74678786d23911947e468feec139d
ameinabdi <ameinabdi@gmail.com>
2019-06-27 11:07:37 +0300

    added

---
+++ src/javaconnection.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+import javax.swing.*;
+public class javaconnection {
+      Connection conn=null;
+      
+              public static Connection ConnectDB(){
+        
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+     Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/amouduniversity","root","");
+    JOptionPane.showMessageDialog(null, "Connected");
+     
+     
+     return conn;
+     
+     
+    } catch ( Exception e ) {
+      JOptionPane.showMessageDialog(null, e);
+      
+      return null;
+    }
+    }
+
+
+
+
+    
+}
